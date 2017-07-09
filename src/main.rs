eb0525d589e60a555e17a01e39066c7d3dd74402
Martin Tomasi <martin.tomasi@gmail.com>
2017-07-09 13:56:37 +0200

    Sort of works!

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate error_chain;
 extern crate petgraph;
 extern crate walkdir;
 extern crate syn;
+extern crate clap;
 
 use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 use std::collections::{HashSet, HashMap};
+use std::ffi::OsStr;
 
 use walkdir::WalkDir;
 use petgraph::prelude::*;
@@ -78,7 +80,7 @@ fn is_rust_file(e: &walkdir::DirEntry) -> bool {
     e.path().extension().map(|e| e == "rs").unwrap_or(false)
 }
 
-fn build_dependency_graph<P>(root_path: P) -> Result<Graph<String, ()>>
+fn build_dependency_graph<P>(root_path: P, ignore_extern: bool) -> Result<Graph<String, ()>>
 where
     P: AsRef<Path>,
 {
@@ -93,13 +95,29 @@ where
         let path = entry.path();
         let file = file_to_ast(path)?;
         let modules = extract_used_modules(&file);
-        let this_module = path.file_stem().unwrap().to_string_lossy().into_owned();
-        let from_idx = nodes.entry(this_module.clone())
-            .or_insert_with(|| graph.add_node(this_module.clone())).clone();
+        let this_module = path.strip_prefix(&root_path)?;
+        let this_module = Path::new(this_module.iter().nth(0).unwrap());
+    
+        let this_module = if this_module.extension() == Some(OsStr::new("rs")) { 
+            let s = format!("{}", this_module.display());
+            let l = s.len();
+            s[..l-3].to_string()
+        } else {
+            format!("{}", this_module.display())
+        };
+
+        let from_idx = nodes
+            .entry(this_module.clone())
+            .or_insert_with(|| graph.add_node(this_module.clone()))
+            .clone();
         for module in &modules {
+            if ignore_extern && is_external_dependency(&root_path, &module) {
+                continue;
+            }
             let to_idx = nodes
                 .entry(module.clone())
-                .or_insert_with(|| graph.add_node(module.clone())).clone();
+                .or_insert_with(|| graph.add_node(module.clone()))
+                .clone();
             if graph.find_edge(from_idx, to_idx).is_none() {
                 graph.add_edge(from_idx, to_idx, ());
             }
@@ -112,8 +130,12 @@ fn main() {
     use std::fs::File;
     use std::io::Write;
 
-    let graph = build_dependency_graph("C:\\Users\\Martin\\IdeaProjects\\link-collector\\src")
+    let graph = build_dependency_graph("C:\\Users\\Martin\\IdeaProjects\\link-collector\\src", true)
         .unwrap();
     let mut file = File::create("graph.dot").unwrap();
-    write!(file, "{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel])).unwrap();
+    write!(
+        file,
+        "{:?}",
+        Dot::with_config(&graph, &[Config::EdgeNoLabel])
+    ).unwrap();
 }
