ad2ee6a0bbf146642d8abcc62688f771cfe09236
Martin Tomasi <martin.tomasi@gmail.com>
2019-03-01 18:44:58 +0100

    bump dependency versions

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
 #[macro_use]
 extern crate clap;
-#[macro_use]
-extern crate error_chain;
+extern crate failure;
 extern crate petgraph;
 extern crate syn;
 extern crate walkdir;
 
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 
-use petgraph::dot::{Dot, Config};
+use petgraph::dot::{Config, Dot};
 use petgraph::prelude::*;
 use walkdir::WalkDir;
 
-use errors::Result;
-
-mod errors {
-    error_chain! {
-        foreign_links {
-            Io(::std::io::Error);
-            Syn(::syn::ParseError);
-            WalkDir(::walkdir::Error);
-            Prefix(::std::path::StripPrefixError);
-        }
-    }
-}
+type Result<T> = std::result::Result<T, failure::Error>;
 
 fn file_to_ast<P>(path: P) -> Result<syn::File>
 where
@@ -40,21 +28,25 @@ where
 }
 
 fn extract_used_modules(file: &syn::File) -> HashSet<String> {
-    let mut statements = HashSet::new();
-    for item in &file.items {
-        if let syn::ItemKind::Use(ref item_use) = item.node {
-            let path = match *item_use.path {
-                syn::ViewPath::Simple(ref p) => &p.path,
-                syn::ViewPath::Glob(ref p) => &p.path,
-                syn::ViewPath::List(ref p) => &p.path,
-            };
-            match path.segments.iter().nth(0) {
-                Some(s) => statements.insert(s.item().ident.to_string()),
-                None => continue,
-            };
+    use syn::visit::Visit;
+
+    struct Visitor {
+        statements: HashSet<String>,
+    }
+
+    impl<'ast> Visit<'ast> for Visitor {
+        fn visit_use_tree(&mut self, tree: &syn::UseTree) {
+            if let syn::UseTree::Path(tree) = tree {
+                self.statements.insert(tree.ident.to_string());
+            }
         }
     }
-    statements
+    let mut visitor = Visitor {
+        statements: Default::default(),
+    };
+    syn::visit::visit_file(&mut visitor, file);
+
+    visitor.statements
 }
 
 fn is_external_dependency<P>(root: P, module: &str) -> bool
@@ -74,23 +66,26 @@ fn is_rust_file(e: &walkdir::DirEntry) -> bool {
 fn module_from_path(root_path: &Path, path: &Path) -> Result<String> {
     let relative_path = path.strip_prefix(root_path)?;
     let module_name = Path::new(relative_path.iter().nth(0).unwrap());
-    Ok(
-        module_name
-            .file_stem()
-            .unwrap()
-            .to_string_lossy()
-            .into_owned(),
-    )
+    Ok(module_name
+        .file_stem()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned())
 }
 
 fn build_dependency_graph<P>(root_path: P, ignore_extern: bool) -> Result<Graph<String, ()>>
 where
     P: AsRef<Path>,
 {
+    use std::ffi::OsStr;
+
     let mut graph = Graph::new();
     let mut nodes = HashMap::new();
 
-    for entry in WalkDir::new(&root_path) {
+    let walker = WalkDir::new(&root_path)
+        .into_iter()
+        .filter_entry(|e| e.path().file_name() != Some(OsStr::new("target")));
+    for entry in walker {
         let entry = entry?;
         if !is_rust_file(&entry) {
             continue;
@@ -119,14 +114,15 @@ where
 }
 
 fn run() -> Result<()> {
-    let matches = clap_app!(depgraph => 
+    let matches = clap_app!(depgraph =>
         (version: "0.1")
         (author: "Martin Tomasi <martin.tomasi@gmail.com>")
         (about: "Shows a dependency graph for Rust projects")
         (@arg IGNORE_EXTERNAL: -i --ignore "Ignore external dependencies (extern crates)")
         (@arg OUT_PATH: +takes_value -o --output "Graphviz output file.")
         (@arg SRC_PATH: +required "Path to the src folder of the Rust project")
-    ).get_matches();
+    )
+    .get_matches();
 
     let path = matches.value_of("SRC_PATH").unwrap();
     let ignore_external = matches.is_present("IGNORE_EXTERNAL");
@@ -143,7 +139,6 @@ fn run() -> Result<()> {
                 }
             }
         }
-
     } else {
         let path = matches.value_of("OUT_PATH").unwrap();
         let mut file = File::create(path)?;
